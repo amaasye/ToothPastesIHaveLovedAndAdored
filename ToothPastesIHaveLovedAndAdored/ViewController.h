8b1a7e463a2285fff1e6eac579c0e4dcfdffce11
Syed Amaanullah <syedamaanullah@Syeds-MacBook-Pro.local>
2015-01-22 10:38:52 -0600

    Initial Commit

---
+++ ToothPastesIHaveLovedAndAdored/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ToothPastesIHaveLovedAndAdored
+//
+//  Created by Syed Amaanullah on 1/22/15.
+//  Copyright (c) 2015 Syed Amaanullah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
